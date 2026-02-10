88d35ab7ac78cecadfa95ee222a48c76f0b3f0de
Natan Gabriel Lopes <naglopes2007@gmail.com>
2026-02-10 11:52:29 -0300

    Implement Text and new button in UI

--- app/src/main/java/com/example/growbyday/DashboardActivity.kt
+++ app/src/main/java/com/example/growbyday/DashboardActivity.kt
@@ -4,6 +4,8 @@ import android.os.Bundle
 import android.util.Log
 import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
+import java.text.NumberFormat
+import java.util.Locale
 import kotlin.math.pow
 
 class DashboardActivity: AppCompatActivity() {
@@ -18,20 +20,21 @@ class DashboardActivity: AppCompatActivity() {
         name.text = "Olá ${username ?: "usuário"}👋🏻, tudo bem?"
 
         val days = findViewById<TextView>(R.id.days)
-
+        val valgoal = findViewById<TextView>(R.id.valgoal)
         fun soma(){
             val a1 = 1
             val r = 1
             val n = totaldays
 
             var term = a1
-            var value =0
+            var value = 0
 
             for(i in 1..n){
                 value += term
                 term += r
             }
-            days.text = "Você estabeleceu a meta para: ${totaldays} dias! Ou seja, no final do desafio você vai ter acumulado R$ ${value},00"
+            days.text = "Você estabeleceu a meta para: ${totaldays} dias! Ou seja, no final do desafio você vai ter acumulado"
+            valgoal.text = "R$ ${value},00"
         }
         soma()
 
